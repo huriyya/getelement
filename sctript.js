2f4084a833424e7c5d84e4e0446175a5f70bd8d6
Huriyye <huriyye@gmail.com>
2024-07-01 18:27:22 -0700

    yeni

---
+++ sctript.js
@@ -0,0 +1,8 @@
+let mesafe = prompt ("mesafeni yazin")
+let suret = prompt ('sureti qeyd edin')
+let seher = prompt ('seheri yazin')
+let vaxt = mesafe/suret
+let netice = (' Ege  Fuzuliden  mesafe ' + mesafe + '  suret  ise ' + suret +  '  km  olarsa  masin' + ' Genceye  ' +  vaxt + '  saat cataxag ')
+document.getElementById ('vaxt').innerHTML = netice
+
+
